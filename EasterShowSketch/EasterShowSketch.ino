304b2fce312d98d6aa14c1275e6a8d69affd114b
Ben Jones <benpaddlejones@outlook.com>
2023-04-03 10:36:59 +1000

    Update EasterShowSketch.ino

--- EasterShowSketch/EasterShowSketch.ino
+++ EasterShowSketch/EasterShowSketch.ino
@@ -12,20 +12,20 @@
 #define DREQ 3       // VS1053 Data request, ideally an Interrupt pin
 
 Adafruit_VS1053_FilePlayer musicPlayer = Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
-  
+
 void setup() {
   pinMode(1, OUTPUT);
   pinMode(2, OUTPUT);
   randomSeed(analogRead(0));
+
   Serial.begin(9600);
-  musicPlayer.setVolume(20,20);
 
   if (! musicPlayer.begin()) { // initialise the music player
      Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
      while (1);
   }
   Serial.println(F("VS1053 found"));
-  
+
    if (!SD.begin(CARDCS)) {
     Serial.println(F("SD failed, or not present"));
     while (1);  // don't do anything more
@@ -33,7 +33,7 @@ void setup() {
 
   // list files
   printDirectory(SD.open("/"), 0);
-  
+
   // Set volume for left, right channels. lower numbers == louder volume!
   musicPlayer.setVolume(1,1);
 
@@ -41,38 +41,33 @@ void setup() {
 }
 
 void loop() {
-  WindMillOn ();
- int randNumber = random(1, 5);
+int randNumber = random(1, 5);
  Serial.println(randNumber);
  if (randNumber == 1)
-	{
-	 musicPlayer.playFullFile("/track002.mp3");
-	 delay(3000);
+  {
+   musicPlayer.playFullFile("/track002.mp3");
+   delay(3000);
  } else if (randNumber == 2)
  {
-	 musicPlayer.playFullFile("/track003.mp3"); 
-	 delay(3000);
+   musicPlayer.playFullFile("/track003.mp3"); 
+   delay(3000);
  } else if (randNumber == 3)
-	 {
-		WindMillOn ();
-		WindMillOff ();
-		delay(3000);
+   {
+    WindMillOn ();
+    WindMillOff ();
+    delay(3000);
  } else if (randNumber == 4)
-	 {
-	 musicPlayer.playFullFile("/track005.mp3");
-	 delay(3000);
+   {
+   musicPlayer.playFullFile("/track005.mp3");
+   delay(3000);
  } else if (randNumber == 5)
-	{
-	 musicPlayer.playFullFile("/track004.mp3");
-	 delay(3000);
+  {
+   musicPlayer.playFullFile("/track004.mp3");
+   delay(3000);
  }
 }
 
-
-
-
 void WindMillOn () {
-  musicPlayer.setVolume(20,20);
   digitalWrite(1, HIGH);  
   digitalWrite(2, HIGH);  
   musicPlayer.playFullFile("/track001.mp3");
@@ -91,7 +86,7 @@ void WindMillOff () {
 
 void printDirectory(File dir, int numTabs) {
    while(true) {
-     
+
      File entry =  dir.openNextFile();
      if (! entry) {
        // no more files
